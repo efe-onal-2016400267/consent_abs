c0bb9b3820a0b06cb397bf4b923e259929bc3611
efe-onal-2016400267 <efeonalll@gmail.com>
2025-07-28 19:02:38 +0300

    testing interface is implemented

--- base_model/model.py
+++ base_model/model.py
@@ -9,6 +9,8 @@ from mesa.experimental.cell_space import OrthogonalVonNeumannGrid
 import yaml
 
 GOAL_FILE_PATH = "./goals/goal_tree.yaml"
+TEST_CASE_PATH = "./test_cases/test_001_1.yaml"
+TEST = True
 
 class NoConsentModel(mesa.Model):
     """
@@ -26,7 +28,7 @@ class NoConsentModel(mesa.Model):
                 ):
         super().__init__(seed=seed)
 
-        # initiate width and height of the grid
+         # initiate width and height of the grid
         self.width = width
         self.height = height
         self.goal_per_agent = goal_per_agent
@@ -35,55 +37,81 @@ class NoConsentModel(mesa.Model):
         self.running = True
         self.grid = OrthogonalVonNeumannGrid(
             (self.width, self.height), torus=False, random=self.random
-        )
+        )   
 
-        # Read the yaml file first to get the goals
-        with open(GOAL_FILE_PATH, 'r') as f:
-            self.all_goals = yaml.safe_load(f)["goals"]
+        if TEST:
+            with open(TEST_CASE_PATH, 'r') as f:
+                test_case_data = yaml.safe_load(f)["agents"]
+                self.goals_of_agents = []
+                self.resources_of_agents = []
 
-        # in resources we hold different resource types extractd from distinct subgoals.
-        # in goals we hold unique goals and their subgoals.
-        self.goals = set()
-        self.resources = set()
+                for agent in test_case_data:
+                    # prepare goal tuples
+                    goals = [(g["name"], tuple(g["sub_goals"])) for g in agent["goals"]]
+                    self.goals_of_agents.append(goals)
 
-        for goal in self.all_goals:
-            subgoals = tuple(sg["name"] for sg in goal["sub_goals"])
-            self.goals.add((goal["name"], subgoals))
-            for sub in subgoals:
-                self.resources.add(sub.split("_")[1])
+                    # prepare resource tuples
+                    # Create Resource instances and assign ownership
+                    resource_instances = [
+                        Resource(name=r["name"], owner=agent["id"], type=r["type"]) for r in agent["resources"]
+                    ]
+                    self.resources_of_agents.append(resource_instances)
 
-        self.goals = list(self.goals)
-        self.resources = list(self.resources)
+                ChefAgent.create_agents(
+                    self,
+                    len(test_case_data),
+                    cell=self.random.choices(self.grid.all_cells.cells, k=len(test_case_data)),
+                    # Now I need to feed goals and sovereign resources at random.
+                    goals = self.goals_of_agents,
+                    sovereigned_resources = self.resources_of_agents
+                )
+        else:
+            # Read the yaml file first to get the goals
+            with open(GOAL_FILE_PATH, 'r') as f:
+                self.all_goals = yaml.safe_load(f)["goals"]
 
-        # now for each agent, we'll create a different set of goals and resources. Let each agent have 2 goals and 2 resources.
-        # TODO: Make sure all resources exist in the model, that is all goals are achievable.
-        # TODO: But we need to make these resources instances of the resource class. How would we do that?
-        self.goals_of_agents = []
-        self.resources_of_agents = []
-        for agent_index in range(initial_population):
-            self.goals_of_agents.append(self.random.sample(self.goals, k=self.goal_per_agent))
+            # in resources we hold different resource types extractd from distinct subgoals.
+            # in goals we hold unique goals and their subgoals.
+            self.goals = set()
+            self.resources = set()
 
-            # Sample resource types (e.g., 'egg', 'oven')
-            resource_types = self.random.sample(self.resources, k=self.resource_per_agent)
+            for goal in self.all_goals:
+                subgoals = tuple(sg["name"] for sg in goal["sub_goals"])
+                self.goals.add((goal["name"], subgoals))
+                for sub in subgoals:
+                    self.resources.add(sub.split("_")[1])
 
-            # Create Resource instances and assign ownership
-            resource_instances = [
-                Resource(name=f"{res_type}_{agent_index}_{i}", owner=agent_index, type=res_type)
-                for i, res_type in enumerate(resource_types)
-            ]
-            self.resources_of_agents.append(resource_instances)
+            self.goals = list(self.goals)
+            self.resources = list(self.resources)
 
-        ChefAgent.create_agents(
-            self,
-            initial_population,
-            cell=self.random.choices(self.grid.all_cells.cells, k=initial_population),
-            # Now I need to feed goals and sovereign resources at random.
-            goals = self.goals_of_agents,
-            sovereigned_resources = self.resources_of_agents
-        )
+            # now for each agent, we'll create a different set of goals and resources. Let each agent have 2 goals and 2 resources.
+            # TODO: Make sure all resources exist in the model, that is all goals are achievable.
+            # TODO: But we need to make these resources instances of the resource class. How would we do that?
+            self.goals_of_agents = []
+            self.resources_of_agents = []
+            for agent_index in range(initial_population):
+                self.goals_of_agents.append(self.random.sample(self.goals, k=self.goal_per_agent))
+
+                # Sample resource types (e.g., 'egg', 'oven')
+                resource_types = self.random.sample(self.resources, k=self.resource_per_agent)
+
+                # Create Resource instances and assign ownership
+                resource_instances = [
+                    Resource(name=f"{res_type}_{agent_index}_{i}", owner=agent_index, type=res_type)
+                    for i, res_type in enumerate(resource_types)
+                ]
+                self.resources_of_agents.append(resource_instances)
+
+            ChefAgent.create_agents(
+                self,
+                initial_population,
+                cell=self.random.choices(self.grid.all_cells.cells, k=initial_population),
+                # Now I need to feed goals and sovereign resources at random.
+                goals = self.goals_of_agents,
+                sovereigned_resources = self.resources_of_agents
+            )
 
     def step(self):
-        #self.agents.do("print_goals_and_resources")
         self.agents.do("interpret_goals")
 
     
